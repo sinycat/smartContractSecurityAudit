92727a163dfcc436c793c2e002a519f8800d8f8b
gmh5225 <2315157@qq.com>
2024-12-01 20:15:58 +0800

    gm

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,36 @@ const nextConfig = {
     domains: ['mush-audit.vercel.app'],
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        // match all routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/etherscan/:path*',
+        destination: 'https://api.etherscan.io/api/:path*',
+      },
+      {
+        source: '/api/bscscan/:path*',
+        destination: 'https://api.bscscan.com/api/:path*',
+      },
+      {
+        source: '/api/arbiscan/:path*',
+        destination: 'https://api.arbiscan.io/api/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
