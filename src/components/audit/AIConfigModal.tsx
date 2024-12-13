0558b3f0d762202009d0e3c6d34cab591f595b92
gmh5225 <2315157@qq.com>
2024-12-13 08:35:11 +0800

    Revert "Set default value of superPrompt to "true""
    This reverts commit e46be2b90dd8ce091a2d24937299ac758931811f.

--- src/components/audit/AIConfigModal.tsx
+++ src/components/audit/AIConfigModal.tsx
@@ -28,7 +28,7 @@ export default function AIConfigModal({
     xaiKey: "",
     selectedModel: GPT_MODELS[0].id,
     language: "english",
-    superPrompt: true,
+    superPrompt: false,
   });
   const providerInfo = getProviderInfo(config.provider);
 
@@ -246,7 +246,7 @@ export default function AIConfigModal({
                 xaiKey: "",
                 selectedModel: GPT_MODELS[0].id,
                 language: "english",
-                superPrompt: true,
+                superPrompt: false,
               });
             }}
             className="px-4 py-2 text-gray-400 hover:text-white transition-colors"
